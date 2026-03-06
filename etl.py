4cc0925c2d1e3e5091f53036ebce1b6e8af7e1d1
lo496 <liamoneill496@gmail.com>
2026-03-06 13:34:48 -0500

    added more transformations

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession, Window
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-from pyspark.sql.functions import col, lit, when, count
+from pyspark.sql.functions import col, lit, when, count, sum, avg, substring
 
 def main():
     spark = SparkSession.builder \
@@ -12,14 +12,14 @@ def main():
 
     # sample data - replace later
     data = [
-        ('1', 'customer 1', 'Apartment', 2000, '2026-02-10', '2026-03-03', 1, 900, 2000, 12, 'rented', 'New York'),
-        ('2', 'customer 2', 'Single Family', 1980, '2025-07-20', '2026-01-30', 3, 2350, 4000, 24, 'rented', 'Los Angeles'),
-        ('3', 'customer 3', 'Apartment', 1970, '2025-10-31', None, 1, 680, 1500, 6, 'open', 'Chicago')
+        ('1', 'customer 1', 'Apartment', 2000, '2026-02-10T09:00:00Z', '2026-03-03T12:32:13Z', 1, 900, 2000, 12, 'rented', 'New York'),
+        ('2', 'customer 2', 'Single Family', 1980, '2025-07-20T12:00:00Z', '2026-01-30T19:45:22Z', 3, 2350, 4000, 24, 'rented', 'Los Angeles'),
+        ('3', 'customer 3', 'Apartment', 1970, '2025-10-31T06:17:55Z', None, 1, 680, 1500, 6, 'open', 'Chicago')
     ]
 
     # 3A
 
-    rdd = sc.parallelize(data) # replace with load from parquet 
+    rdd = sc.parallelize(data) # TODO replace with load from parquet 
 
     rdd_rented = rdd.filter(lambda x: x[10] == 'rented')
     # rdd_rented.saveAsTextFile('rented_listings')
@@ -44,14 +44,24 @@ def main():
         StructField('city', StringType(), True),
     ])
 
-    df = spark.createDataFrame(data, schema) # replace with load from parquet 
+    df = spark.createDataFrame(data, schema).cache() # TODO replace with load from parquet, add parttioning/bucketing
+    df.count()
 
-    # TODO Hourly Rentals Summary
+
+    # Hourly Rentals Summary
+    hourly_rentals = df.withColumn('revenue', col('rent') * col('duration')).withColumn('rented_hour', substring(col('rented_on'), 0, 13)).groupBy(col('rented_hour')).agg(
+        count('*').alias('total_rentals'),
+        sum('revenue').alias('total_revenue'),
+        avg('revenue').alias('average_rental_value')
+    )
+    # hourly_rentals.show()
+
 
     # Rentals by Bedroom Count
     window_partition = Window.partitionBy("bedrooms")
     rentals_by_bedrooms = df.withColumn('rentals_by_bedrooms', count(col('property_id')).over(window_partition))
     # rentals_by_bedrooms.show()
+
 
     # State Revenue
     city_state_schema = StructType([
@@ -71,7 +81,10 @@ def main():
     revenue_by_state = spark.sql('SELECT s.state, SUM(l.rent*l.duration) AS revenue FROM (listings l INNER JOIN states s ON l.city = s.city) GROUP BY s.state')
     # revenue_by_state.show()
 
-    # TODO Rental Status Breakdown
+
+    # Rental Status Breakdown
+    status_breakdown = df.groupBy('building_type').pivot('rental_status', ['rented', 'open']).agg(count('rental_status')).fillna(0)
+    # status_breakdown.show()
 
 
 if __name__ == "__main__":
