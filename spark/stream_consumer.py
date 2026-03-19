730210f9314aed4a248b903c28abfbe5391bb272
lo496 <liamoneill496@gmail.com>
2026-03-19 14:08:16 -0400

    etls reading from parquet writing blank

--- spark/stream_consumer.py
+++ spark/stream_consumer.py
@@ -4,13 +4,13 @@ def main():
     # Create Spark session
     spark = SparkSession.builder \
         .appName("Real Estate Data Pipeline") \
-        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:4.1.1") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
         .getOrCreate()
 
     # Read from Kafka topic as a streaming DataFrame
     kafka_df = spark.readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("kafka.bootstrap.servers", "kafka:9092") \
         .option("subscribe", "listing-events") \
         .load()
 
@@ -18,13 +18,12 @@ def main():
     # The value column contains the actual message (as bytes)
     messages = kafka_df.selectExpr("CAST(value AS STRING) as message")
 
-    # Write to console (for demonstration)
     query = messages.writeStream \
         .trigger(processingTime="10 seconds") \
         .outputMode("append") \
         .format("parquet") \
-        .option("path", "/opt/airflow/data") \
-        .option("checkpointLocation", "/opt/airflow/data/tmp/checkpoint") \
+        .option("path", "/opt/data/raw") \
+        .option("checkpointLocation", "/opt/data/tmp/checkpoint") \
         .start()
 
     query.awaitTermination()
