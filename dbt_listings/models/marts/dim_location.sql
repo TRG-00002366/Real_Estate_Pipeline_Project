2dc3272dc4491ed748530d42e1dbd8e532b19aea
Denis Dudkin <denis.dudkin.d@gmail.com>
2026-03-27 15:58:34 -0400

    Changed surrogate key to de dupe

--- dbt_listings/models/marts/dim_location.sql
+++ dbt_listings/models/marts/dim_location.sql
@@ -1,7 +1,7 @@
 WITH listings AS (SELECT * FROM {{ref('stg_listings_enriched')}})
 
 SELECT distinct
-    {{ dbt_utils.generate_surrogate_key(['listings.city']) }} AS location_key,
+    {{ dbt_utils.generate_surrogate_key(['listings.city','listings.state']) }} AS location_key,
     listings.city,
     listings.state
 FROM listings
